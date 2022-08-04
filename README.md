07b84eddbb1e0f39f7b63af837a982d941fd151b
ranjan-abhijeet <ranjan.viper@outlook.com>
2022-08-04 12:52:35 +0530

    updated README and added code to fetch particulate matter forecasts

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ecmwf-pm25
 PM 2.5 data fetcher, source: ECMWF
+
+This script downloads data from CAMS Global Atmospheric Composition Forecasts. 
+
+To use this code you need to have access to ECMWF.
+
+Visit: https://ads.atmosphere.copernicus.eu/api-how-to to create an account
+and get your login credentials, username and api key.
+
+For windows server, use the following article:
+    https://confluence.ecmwf.int/display/CKB/How+to+install+and+use+CDS+API+on+Windows
+
+For linux server, use the following article:
+    https://ads.atmosphere.copernicus.eu/api-how-to
---
+++ main.py
@@ -0,0 +1,64 @@
+import cdsapi
+import xarray as xr
+from datetime import datetime, timedelta
+
+
+cds_client = cdsapi.Client()
+
+# Date from which we want the forecasts to start.
+# Final query format is as per the requirement of cdsapi
+
+query_date = datetime.now() - timedelta(days=1)
+date = query_date.strftime("%Y-%m-%d")
+cds_download_date = date + "/" + date
+
+# The data product we are using for PM 1.0, PM 2.5, and PM 10.0 forcasts
+product = 'cams-global-atmospheric-composition-forecasts'
+
+# Format of downloading data, other option is netcdf
+data_download_format = 'grib' 
+
+# Forecasted lead time in hours.. asks for queries for next 120 hours
+lead_time = [str(i) for i in range(1, 121)]
+
+# Variables we want to forecast, PM 1.0, PM 2.5, and PM 10.0 in this case
+forecasted_variables = [
+                        'particulate_matter_10um',
+                        'particulate_matter_2.5um',
+                        'particulate_matter_1um'
+                        ]
+
+# Hour from which forecast is required.
+time = '00:00'  # other option is '12:00'
+
+# Bounding box for query region.. India at the moment
+# [north, west, south, east]
+bounding_box = [38, 68, 7, 98]
+
+print("[+] Retrieving data from CAMS Global Atmospheric Composition Forecasts")
+
+try:
+    cds_client.retrieve(
+        product,
+        {
+            'date': cds_download_date,
+            'type': 'forecast',
+            'format': data_download_format,
+            'variable': forecasted_variables,
+            'time': '00:00',
+            'leadtime_hour':lead_time,
+            'area': bounding_box,
+        },
+        f'{date}.grib')    # name of the .grib file
+
+    print(f"[+] Reading download {date}.grib file")
+    ds = xr.open_dataset(f'{date}.grib')
+
+    print(f"[+] Converting .grib to .csv file format")
+    df = ds.to_dataframe()
+
+    df.to_csv(f'{date}.csv')
+    print(f"[+] Downloaded data in .csv file")
+
+except:
+    print("[-] Work on this")
