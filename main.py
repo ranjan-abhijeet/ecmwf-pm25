bd267a423f061081c45f67fae2240499db58afa1
ranjan-abhijeet <ranjan.viper@outlook.com>
2022-10-03 15:57:02 +0530

    refactor code to provide one dataset with the hourly pm2.5 data

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import xarray as xr
 from datetime import datetime, timedelta
 from pre_processor import extrapolator
 
+
+logger = utils.get_logger()
 cds_client = cdsapi.Client()
+logger.info('cdsapi client instantiated')
 
 # Date from which we want the forecasts to start.
 # Final query format is as per the requirement of cdsapi
@@ -13,66 +16,77 @@ query_delay = 1 # delay in query
 query_date = datetime.now() - timedelta(days=query_delay)
 date = query_date.strftime("%Y-%m-%d")
 cds_download_date = date + "/" + date
+logger.info(f'downloading data for {cds_download_date}')
 
 # The data product we are using for PM 1.0, PM 2.5, and PM 10.0 forcasts
-product = "cams-global-atmospheric-composition-forecasts"
+PRODUCT = "cams-global-atmospheric-composition-forecasts"
+# Type of data which needs to be fetched
+TYPE = 'forecast'
 
 # Format of downloading data, other option is netcdf
 data_download_format = "grib" 
 
 # Forecasted lead time in hours.. asks for queries for next 24 hours
-# lead_time = [str(i) for i in range(0, 24)]
-# Need to work on the logic, staying with 36th hour forecast for now
-lead_time = ["36"]
+lead_time = [str(i) for i in range(24, 27)]
+
 # Variables we want to forecast, PM 1.0, PM 2.5, and PM 10.0 in this case
 forecasted_variables = [
-                        # 'particulate_matter_10um',
                         "particulate_matter_2.5um",
-                        # 'particulate_matter_1um'
                         ]
 
 # Hour from which forecast is required.
 time = "00:00"  # other option is '12:00'
 
-# Bounding box for query region.. India at the moment
-# [north, west, south, east]
+# Bounding box for query region.. India for this context
+# [north, west, south, east] latitudes and longitudes
 bounding_box = [38, 68, 7, 98]
 
 # path of downloaded data
-csv_path = f"downloaded_data/{date}.csv"
-grib_path = f"downloaded_data/{date}.grib"
-interpolated_csv_path = "output_data/pm2.5.csv"
+data_download_home_path = "downloaded_data"
+interpolated_data_home_path = "output_data"
 
 # path of coordinates where we want to extrapolate
 india_coordinates_path = "india_coordinates/india_coordinates.csv" 
 
-print("[+] Retrieving data from CAMS Global Atmospheric Composition Forecasts")
 
-try:
-    cds_client.retrieve(
-        product,
-        {
-            'date': cds_download_date,
-            'type': 'forecast',
-            'format': data_download_format,
-            'variable': forecasted_variables,
-            'time': '00:00',
-            'leadtime_hour':lead_time,
-            'area': bounding_box,
-        },
-        grib_path)    # name of the .grib file
-    
-    print(f"[+] Processing data")
-    utils.grib_to_csv(grib_file_path=grib_path, csv_file_path=csv_path)
-    df = utils.cleanup_dataframe(csv_file_path=csv_path)
-    output = extrapolator(df, india_coordinates_path)
-    output.to_csv(interpolated_csv_path, index=False)
+def download_data():
+    try:
+        data = pd.read_csv(india_coordinates_path)
+        for lead in lead_time:
+            csv_path = f"{data_download_home_path}/{date}_{lead}.csv"
+            grib_path = f"{data_download_home_path}/{date}_{lead}.grib"
+            output_path = f"{interpolated_data_home_path}/pm25_{lead}.csv"
 
-    print("[+] Cleaning up")
-    utils.remove_file(parent_folder= "downloaded_data", extension="grib")
-    utils.remove_file(parent_folder="downloaded_data", extension="csv")
-    print("[+] Task complete")
-    
-except Exception as err:
-    print("[-] Kindly check the following: ")
-    print(f"[-] {err}")
+            cds_client.retrieve(
+                PRODUCT,
+                {
+                    'date': cds_download_date,
+                    'type': TYPE,
+                    'format': data_download_format,
+                    'variable': forecasted_variables,
+                    'time': time,
+                    'leadtime_hour':lead,
+                    'area': bounding_box,
+                },
+                grib_path)    # name of the .grib file
+            
+            logger.info(f'processing downloaded data for date {date} hour {int(lead) - 24}')
+            utils.grib_to_csv(grib_file_path=grib_path, csv_file_path=csv_path)
+            df = utils.cleanup_dataframe(csv_file_path=csv_path)
+            output = extrapolator(df, india_coordinates_path)
+            data[lead] = output
+            logger.info('processing complete')
+            
+            logger.info('cleaning up local files')
+            utils.remove_file(parent_folder= "downloaded_data", extension="grib")
+            utils.remove_file(parent_folder="downloaded_data", extension="csv")
+            
+            logger.info(f'data downloaded for date {date} hour {int(lead) - 24}')
+        data.to_csv("testfile.csv", index=False)
+    except Exception as err:
+        logger.exception(err)
+        print('[-] Error')
+
+
+if __name__=="__main__":
+    download_data()
