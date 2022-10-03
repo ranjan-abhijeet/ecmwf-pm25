344c528b3ce264bbd87c73cfae4da2197626873b
ranjan-abhijeet <ranjan.viper@outlook.com>
2022-10-03 15:57:30 +0530

    refactor code to transmit only the PM2.5 values

--- pre_processor.py
+++ pre_processor.py
@@ -1,7 +1,10 @@
+import utils
 import pandas as pd
 import scipy.interpolate as interpolator
 import numpy as np
 from glob import glob
+
+logger = utils.get_logger()
 
 def extrapolator(input_data, to_extrapolate_data_path, model="NearestNDInterpolator"):
     """
@@ -11,18 +14,19 @@ def extrapolator(input_data, to_extrapolate_data_path, model="NearestNDInterpola
         input_data: The input data is dataframe created from the downloaded ECMWF data
         to_extrapolate_data_path: The CSV file path which contains the coordinates at which we want to extrapolate.
     """
-    print('[+] Interpolating data')
+    logger.info('interpolating data')
     output = pd.read_csv(to_extrapolate_data_path)
 
     x = np.array(input_data["longitude"])
     y = np.array(input_data["latitude"])
-    z = np.array(input_data["pm2.5"])
+    z = np.array(input_data["pm25"])
 
     interpolate = interpolator.NearestNDInterpolator(list(zip(x, y)), z)
 
     xi = output["longitude"]
     yi = output["latitude"]
-    output["pm2.5"] = 1e+9 * interpolate(xi, yi) # converting kg/m3 to ug/m3
+    output["pm25"] = 1e+9 * interpolate(xi, yi) # converting kg/m3 to ug/m3
+    output.drop(['longitude', 'latitude'], inplace=True)
     output.dropna(inplace=True)
-    print('[+] Interpolated data')
+    logger.info('data interpolated')
     return output
